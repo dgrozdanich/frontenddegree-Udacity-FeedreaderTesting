d0f1ef55bac6d61102816e2398c3f7f14f919599
Dylan Grozdanich <dgrozdanich@gmail.com>
2018-11-06 23:32:22 -0800

    Fixed test
    Fixed one test.
    One more test passes, but is incorrectly passing whilst the array is actually empty and is not testing the proper procedure.
    
    Also added some formatting to the readme file.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,7 +27,7 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* A test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
@@ -41,7 +41,7 @@ $(function() {
          });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* A test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
@@ -55,9 +55,8 @@ $(function() {
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
     describe('the menu', function() {
-        /* TODO: Write a test that ensures the menu element is
+        /* A test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
@@ -67,11 +66,11 @@ $(function() {
          //defines the document body and looks for the menu icon
 
          it('default menu hidden', function() {
-            expect(body.className).toContain('menu-hidden');
+            expect(body.classList.contains('menu-hidden'));
          });
          //checks to make sure the class menu-hidden is added to the body by default
 
-         /* TODO: Write a test that ensures the menu changes
+         /* A test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
@@ -87,9 +86,9 @@ $(function() {
 
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    
     describe('Initial Entries', function() {
-        /* TODO: Write a test that ensures when the loadFeed
+        /* A test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
@@ -109,9 +108,9 @@ $(function() {
         });
 
     });
-    /* TODO: Write a new test suite named "New Feed Selection" */
+
     describe('New Feed Selection', function() {
-        /* TODO: Write a test that ensures when a new feed is loaded
+        /* A test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
